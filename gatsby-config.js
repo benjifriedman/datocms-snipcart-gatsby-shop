40ea23ca65d95d9c3595b88bf2ee3b97c301d1f1
Benji <benjifriedman@gmail.com>
2023-03-16 21:14:00 -0700

    asdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    siteName: 'My Shop',
+    siteName: "Benji Friedman Shop",
   },
   plugins: [
-    'gatsby-plugin-sass',
+    "gatsby-plugin-sass",
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken:
-          process.env.DATO_READONLY_API_KEY || '1b2fe1bfc8abf2e08343a75e444b4d',
+        apiToken: process.env.DATO_READONLY_API_KEY,
       },
     },
     {
-      resolve: 'gatsby-plugin-snipcart-advanced',
+      resolve: "gatsby-plugin-snipcart-advanced",
       options: {
-        publicApiKey:
-          process.env.SNIPCART_PUBLIC_API_KEY ||
-          'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
+        publicApiKey: process.env.SNIPCART_PUBLIC_API_KEY,
         autopop: true,
       },
     },
